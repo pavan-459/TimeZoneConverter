2b5c83a6e9f0b5dbafda247af878db8cbb9bbc23
pavan-459 <124235802+pavan-459@users.noreply.github.com>
2023-05-01 16:26:30 +0530

    added finland, china and jordan

--- src/TimeZoneConverter.js
+++ src/TimeZoneConverter.js
@@ -34,10 +34,14 @@ function TimeZoneConverter() {
       <div>
         <label>Time Zone</label>
         <select value={selectedTimezone} onChange={handleTimezoneChange}>
+          <option value="">Select a time zone</option>
           <option value="America/New_York">New York</option>
           <option value="America/Los_Angeles">Los Angeles</option>
           <option value="Europe/London">London</option>
           <option value="Asia/Tokyo">Tokyo</option>
+          <option value="Asia/Shanghai">China</option>
+          <option value="Europe/Helsinki">Finland</option>
+          <option value="Asia/Amman">Jordan</option>
         </select>
       </div>
       <button onClick={convertTimezone}>Convert Time</button>
